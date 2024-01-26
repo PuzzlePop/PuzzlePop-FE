34f0b2d0827e0b5e7358d955f26c1b6d92d738b0
용상윤 <tkddbs1644@gmail.com>
2024-01-26 16:58:01 +0900

    test: socket 연결 테스트 중

--- src/pages/CooperationGame/CooperationGameListPage.jsx
+++ src/pages/CooperationGame/CooperationGameListPage.jsx
@@ -1,5 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import GamePageNavigation from "../../components/GamePageNavigation";
+
+const request = axios.create({
+  baseURL: "http://localhost:8080",
+  timeout: 3000,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+});
 
 export default function CooperationGameListPage() {
   const [roomList, setRoomList] = useState([]);
@@ -19,39 +27,55 @@ export default function CooperationGameListPage() {
 const SocketTestComponent = () => {
   const [roomTitle, setRoomTitle] = useState("");
   const [userId, setUserId] = useState("");
+  const [roomList, setRoomList] = useState([]);
 
-  const handleCreateWaitingRoom = () => {
+  const createRoom = async () => {
     if (!roomTitle || !userId) {
       return;
     }
 
-    // TODO: 여기서 소켓 연결
-    window.alert(
-      JSON.stringify({
-        roomTitle,
-        userId: Number(userId),
-      }),
-    );
+    // TODO: CORS 에러 해결하기
+    const response = await request.post(`/game/room`, {
+      name: roomTitle,
+      userid: userId,
+    });
+
+    console.log(response);
   };
+
+  const findAllRoom = async () => {
+    axios.get("/game/rooms").then((response) => {
+      if (response.status === 200 && response.data) {
+        setRoomList(response.data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    findAllRoom();
+  }, []);
 
   return (
     <>
-      <input
-        placeholder="방 제목"
-        value={roomTitle}
-        onChange={(e) => {
-          setRoomTitle(e.target.value);
-        }}
-      />
-      <input
-        type="number"
-        placeholder="유저 아이디"
-        value={userId}
-        onChange={(e) => {
-          setUserId(e.target.value);
-        }}
-      />
-      <button onClick={handleCreateWaitingRoom}>방 생성하기</button>
+      <div>
+        <input
+          placeholder="방 제목"
+          value={roomTitle}
+          onChange={(e) => {
+            setRoomTitle(e.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="유저 아이디"
+          value={userId}
+          onChange={(e) => {
+            setUserId(e.target.value);
+          }}
+        />
+        <button onClick={createRoom}>방 생성하기</button>
+      </div>
+      <div></div>
     </>
   );
 };
--- src/pages/CooperationGame/CooperationGameWaitingPage.jsx
+++ src/pages/CooperationGame/CooperationGameWaitingPage.jsx
@@ -1,3 +1,12 @@
+import { useParams } from "react-router-dom";
+
 export default function CooperationGameWaitingPage() {
-  return <h1>CooperationGameWaitingPage</h1>;
+  const { roomId } = useParams();
+
+  return (
+    <>
+      <h1>CooperationGameWaitingPage</h1>
+      <div>roomId : {roomId}</div>
+    </>
+  );
 }
