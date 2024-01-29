26e57b3b7bb11929bb381f4e5b704a9fd71f7f10
dlwndus0728 <dlwndus0728@ajou.ac.kr>
2024-01-29 17:53:25 +0900

    feat: Add send, subscribe function

--- src/pages/CooperationGame/CooperationGameWaitingPage.jsx
+++ src/pages/CooperationGame/CooperationGameWaitingPage.jsx
@@ -9,6 +9,7 @@ export default function CooperationGameWaitingPage() {
   const [roomInfo, setRoomInfo] = useState(null);
   const [sender, setSender] = useState(null);
   const [stompClient, setStompClient] = useState(null);
+  const [messages, setMessages] = useState('');
 
   const findRoom = async () => {
     request.get(`/game/room/${roomId}`).then((res) => setRoomInfo(res.data));
@@ -29,6 +30,21 @@ export default function CooperationGameWaitingPage() {
     stomp.connect({}, () => {
       console.log("WebSocket 연결 성공");
       setStompClient(stomp);
+
+      stomp.subscribe(`/topic/game/room/${roomId}`, (message) => {
+        const newMessage = JSON.parse(message.body);
+        setMessages((prevMessages) => [...prevMessages, newMessage]);
+
+        // 서버에서 받은 game 객체를 콘솔에 출력
+        console.log("Received game object:", newMessage);
+      });
+
+      // 서버로 메시지 전송
+      stomp.send("/app/game/message", {}, JSON.stringify({
+        type: 'ENTER',
+        roomId: roomId,
+        sender: storedSender
+      }));
     });
 
     // 컴포넌트 언마운트 시 연결 종료
@@ -42,10 +58,39 @@ export default function CooperationGameWaitingPage() {
     // eslint-disable-next-line
   }, []);
 
+
+  const handleMessageSend = () => {
+    if (stompClient && sender) {
+      const message = {
+        sender: sender.toString(),
+        content: messages.body,
+      };
+      stompClient.send(`/app/room/${roomId}`, {}, JSON.stringify(message));
+    }
+  };
+
+
   return (
     <>
       <h1>CooperationGameWaitingPage</h1>
       <div>roomId : {roomId}</div>
+
+      {/* <div>
+        <h2>Messages</h2>
+        <ul>
+          {messages.map((message, index) => (
+            <li key={index}>
+              <strong>{message.sender}: </strong>
+              {message.content}
+            </li>
+          ))}
+        </ul>
+      </div> */}
+
+      <div>
+        <input type="text" placeholder="Type your message" onChange={(e) => setMessages(e.target.value)}/>
+        <button onClick={handleMessageSend}>Send</button>
+      </div>
     </>
   );
 }
