1d38f99a13686e050dc4e34fbdc23a5d31b71c11
나해란 <nh2545@naver.com>
2024-01-29 13:04:03 +0900

    feat: nav bar component

--- src/components/GamePageNavigation.jsx
+++ src/components/GamePageNavigation.jsx
@@ -1,11 +1,20 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
 export default function GamePageNavigation() {
+  const url = useLocation().pathname;
+  const [value, setValue] = useState(url);
+
   return (
-    <div style={{ display: "flex", gap: "10px" }}>
-      <Link to="/game/single">싱글</Link>
-      <Link to="/game/cooperation">협동</Link>
-      <Link to="/game/battle">배틀</Link>
-    </div>
+    <Box sx={{ width: "100vw", borderBottom: 1, borderColor: "divider", marginBottom: "50px" }}>
+      <Tabs value={value}>
+        <Tab label="싱글" value="/game/single" component={Link} to="/game/single" />
+        <Tab label="협동" value="/game/cooperation" component={Link} to="/game/cooperation" />
+        <Tab label="배틀" value="/game/battle" component={Link} to="/game/battle" />
+      </Tabs>
+    </Box>
   );
 }
