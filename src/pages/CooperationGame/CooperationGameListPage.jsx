e4512082f0a6ef085e6008a6a944a228631623a1
yong <tkddbs1644@gmail.com>
2024-01-31 08:39:03 +0900

    feat: 협동플레이 목록 페이지 모달 추가

--- src/pages/CooperationGame/CooperationGameListPage.jsx
+++ src/pages/CooperationGame/CooperationGameListPage.jsx
@@ -4,12 +4,23 @@ import GamePageNavigation from "../../components/GamePageNavigation";
 import Header from "../../components/Header";
 import { request } from "../../apis/requestBuilder";
 import GameRoomListBoard from "@/components/GameRoomList/GameRoomListBoard";
+import { Modal, Typography, Box } from "@mui/material";
+
+const USER_ID = "1"; // sample
 
 export default function CooperationGameListPage() {
   const navigate = useNavigate();
-  const userId = 1;
   const [roomList, setRoomList] = useState([]);
   const [roomTitle, setRoomTitle] = useState("");
+  const [roomCount, setRoomCount] = useState(1);
+  const [isOpenedModal, setIsOpenedModal] = useState(false);
+
+  const handleRoomCount = (e) => {
+    const count = Number(e.target.value);
+    if (1 <= count && count <= 6) {
+      setRoomCount(count);
+    }
+  };
 
   const enterRoom = async (roomId) => {
     const sender = window.prompt("닉네임을 입력해주세요");
@@ -21,7 +32,15 @@ export default function CooperationGameListPage() {
   };
 
   const createRoom = async () => {
-    const res = await request.post("/game/room", { name: roomTitle, userid: userId, type: "TEAM" });
+    if (!roomTitle) {
+      return;
+    }
+
+    const res = await request.post("/game/room", {
+      name: roomTitle,
+      userid: USER_ID,
+      type: "TEAM",
+    });
     const { gameId, gameName, gameType, admin, redTeam, blueTeam, isStarted } = res;
     console.log(res);
     setRoomTitle("");
@@ -42,12 +61,7 @@ export default function CooperationGameListPage() {
     <>
       <Header />
       <GamePageNavigation />
-      <input
-        placeholder="방 제목"
-        value={roomTitle}
-        onChange={(e) => setRoomTitle(e.target.value)}
-      />
-      <button onClick={createRoom}>방 만들기</button>
+      <button onClick={() => setIsOpenedModal(true)}>방 만들기</button>
       <ul>
         {roomList.map((room, index) => (
           <div key={index} onClick={() => enterRoom(room.gameId)}>
@@ -56,6 +70,42 @@ export default function CooperationGameListPage() {
         ))}
       </ul>
       <GameRoomListBoard category="cooperation" />
+      <Modal
+        open={isOpenedModal}
+        onClose={() => setIsOpenedModal(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "1px solid #ccc",
+            borderRadius: "10px",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Typography id="modal-modal-title" variant="h4" component="h2">
+            협동방 만들기
+          </Typography>
+          <Box id="modal-modal-description" sx={{ mt: 2 }}>
+            <input
+              placeholder="방 제목"
+              value={roomTitle}
+              onChange={(e) => setRoomTitle(e.target.value)}
+            />
+            <input type="number" value={roomCount} onChange={handleRoomCount} />
+            <button disabled={!roomTitle} onClick={createRoom}>
+              방 만들기
+            </button>
+          </Box>
+        </Box>
+      </Modal>
     </>
   );
 }
