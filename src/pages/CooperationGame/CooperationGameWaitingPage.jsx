6b67a96ed26e73e4ca39e045070d70697a488ed0
용상윤 <tkddbs1644@gmail.com>
2024-01-31 13:21:21 +0900

    feat: ItemController 컴포넌트 스켈레톤 구현

--- src/pages/CooperationGame/CooperationGameWaitingPage.jsx
+++ src/pages/CooperationGame/CooperationGameWaitingPage.jsx
@@ -3,18 +3,19 @@ import { useParams } from "react-router-dom";
 import { request } from "../../apis/requestBuilder";
 import SockJS from "sockjs-client";
 import StompJS from "stompjs";
-import PlayPuzzle from "@/components/PlayPuzzle"
+import PlayPuzzle from "@/components/PlayPuzzle";
+import ItemController from "../../components/ItemController";
 
 export default function CooperationGameWaitingPage() {
   const { roomId } = useParams();
   const [roomInfo, setRoomInfo] = useState(null);
   const [sender, setSender] = useState(null);
   const [stompClient, setStompClient] = useState(null);
-  const [messages, setMessages] = useState('');
+  const [messages, setMessages] = useState("");
 
   useEffect(() => {
-    const storedSender = localStorage.getItem('wschat.sender');
-    const storedRoomId = localStorage.getItem('wschat.roomId');
+    const storedSender = localStorage.getItem("wschat.sender");
+    const storedRoomId = localStorage.getItem("wschat.roomId");
 
     setSender(storedSender);
     let socket;
@@ -22,18 +23,19 @@ export default function CooperationGameWaitingPage() {
     let subscription;
 
     // 방 정보 가져오기
-    request.get(`/game/room/${storedRoomId}`)
+    request
+      .get(`/game/room/${storedRoomId}`)
       .then((res) => {
-        console.log('Room info:', res.data);
+        console.log("Room info:", res.data);
         setRoomInfo(res.data);
-        
+
         // WebSocket 연결
         socket = new SockJS(`http://localhost:8080/game`);
         stomp = StompJS.over(socket);
 
         // 연결 시도
         stomp.connect({}, () => {
-          console.log('WebSocket 연결 성공');
+          console.log("WebSocket 연결 성공");
           setStompClient(stomp);
 
           // 메시지 구독
@@ -42,29 +44,31 @@ export default function CooperationGameWaitingPage() {
             setMessages((prevMessages) => [...prevMessages, newMessage]);
 
             // 서버에서 받은 game 객체를 콘솔에 출력
-            console.log('Received game object:', newMessage);
+            console.log("Received game object:", newMessage);
           });
 
           // 서버로 메시지 전송
-          stomp.send('/app/game/message', {}, JSON.stringify({
-            type: 'ENTER',
-            roomId: storedRoomId,
-            sender: storedSender
-          }));
+          stomp.send(
+            "/app/game/message",
+            {},
+            JSON.stringify({
+              type: "ENTER",
+              roomId: storedRoomId,
+              sender: storedSender,
+            }),
+          );
 
           // 컴포넌트 언마운트 시 연결 종료
-          
         });
       })
-      .catch((error) => console.error('Error fetching room info:', error));
-      return () => {
-        subscription.unsubscribe();
-        stomp.disconnect();
-        console.log('WebSocket 연결 종료');
-      };
+      .catch((error) => console.error("Error fetching room info:", error));
+    return () => {
+      subscription.unsubscribe();
+      stomp.disconnect();
+      console.log("WebSocket 연결 종료");
+    };
     // eslint-disable-next-line
   }, []);
-
 
   const handleMessageSend = () => {
     if (stompClient && sender) {
@@ -79,12 +83,12 @@ export default function CooperationGameWaitingPage() {
   };
 
   const handleInput = (e) => {
-      setMessages(e.target.value);
-  }
-
+    setMessages(e.target.value);
+  };
 
   return (
     <>
+      <ItemController />
       <h1>CooperationGameWaitingPage</h1>
       <div>roomId : {roomId}</div>
 
@@ -101,10 +105,10 @@ export default function CooperationGameWaitingPage() {
       </div> */}
 
       <div>
-        <input type="text" placeholder="Type your message" onChange={handleInput}/>
+        <input type="text" placeholder="Type your message" onChange={handleInput} />
         <button onClick={handleMessageSend}>Send</button>
       </div>
-      <PlayPuzzle/>
+      <PlayPuzzle />
     </>
   );
 }
