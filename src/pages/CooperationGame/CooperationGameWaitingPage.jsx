4c0b00a40112a0cc96b20d1e692d52044f2dda2a
dlwndus0728 <dlwndus0728@ajou.ac.kr>
2024-02-02 16:12:06 +0900

    feat: 채팅 기능 추가

--- src/pages/CooperationGame/CooperationGameWaitingPage.jsx
+++ src/pages/CooperationGame/CooperationGameWaitingPage.jsx
@@ -14,6 +14,10 @@ export default function CooperationGameWaitingPage() {
   const { roomId } = useParams();
   const [gameData, setGameData] = useState(null);
 
+  const [message, setMessage] = useState("");
+  const [chatHistory, setChatHistory] = useState([]); // 채팅 기록을 저장하는 상태 추가
+  
+
   const connectSocket = async () => {
     // websocket 연결 시도
     connect(() => {
@@ -27,8 +31,15 @@ export default function CooperationGameWaitingPage() {
           window.location.href = `/game/cooperation/ingame/${gameId}`;
           return;
         }
-
         setGameData(fetchedGameData);
+      });
+
+      subscribe(`/topic/chat/room/${roomId}`, (message) => {
+        // const { admin, gameId, gameName, picture, redTeam, roomSize, started, ...fetchedGameData } =
+        //   JSON.parse(message.body);
+          const {userid, chatMessage, time} = JSON.parse(message.body);
+          const receivedMessage = { userid: userid, chatMessage: chatMessage, time: time}; // 받은 채팅
+          setChatHistory(prevChat => [...prevChat, receivedMessage]); // 채팅 기록에 새로운 채팅 추가
       });
 
       // 서버로 메시지 전송
@@ -78,22 +89,72 @@ export default function CooperationGameWaitingPage() {
     }
   };
 
+  
+
   if (loading) {
     return <h1>대기실에 입장 중...</h1>;
   }
 
   console.log(gameData);
 
+  /*
+  ***************************채팅 로직*********************************** 
+  */
+  
+
+
+  
+  const handleMessageSend = (e) => {
+    e.preventDefault()  
+    if (getSender()) {
+      send(
+        `/app/game/message`,
+        {},
+        JSON.stringify({
+          roomId: getRoomId(),
+          sender: getSender(),
+          message,
+          type: "CHAT",
+        }),
+      );
+      setMessage("")
+    }
+  };
+
   return (
     <>
       <Header />
       <GamePageNavigation />
-      <SocketMessageTestComponent />
+      
       <h1>CooperationGameWaitingPage</h1>
       <div>roomId : {roomId}</div>
       <div>
         <button onClick={handleGameStart}>GAME START</button>
       </div>
+      
+      {/*채팅 관련*/}
+      <div>
+        <div style={{ marginBottom: '10px' }}>
+          {/* 채팅 기록을 화면에 출력 */}
+          {chatHistory.map((chat, index) => (
+            <div key={index}>
+              <strong>{chat.userid}[{chat.time}]: </strong>{chat.chatMessage}
+            </div>
+          ))}
+        </div>
+
+        <form onSubmit={handleMessageSend}>
+        <input
+          type="text"
+          placeholder="채팅"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button type="submit">Send</button>
+        </form>
+      </div>
+      {/*채팅 끝 */}
+
       {/* <GameWaitingBoard data={dummyData} allowedPiece={allowedPiece} category="cooperation" />{" "} */}
       <Footer />
     </>
@@ -141,33 +202,3 @@ const dummyData = {
   ],
 };
 const allowedPiece = [100, 200, 300, 400, 500];
-
-const SocketMessageTestComponent = () => {
-  const [message, setMessage] = useState("");
-
-  const handleMessageSend = () => {
-    if (getSender()) {
-      send(
-        `/app/game/message`,
-        {},
-        JSON.stringify({
-          roomId,
-          sender: getSender(),
-          message,
-          type: "GAME",
-        }),
-      );
-    }
-  };
-
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="Type your message(test 용)"
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button onClick={handleMessageSend}>Send</button>
-    </div>
-  );
-};
