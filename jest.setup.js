f5a67ea9d4c03a28c3b1cc7a8c4b7904a25b320f
용상윤 <tkddbs1644@gmail.com>
2024-02-02 17:58:27 +0900

    feat: jest 설치 및 parsePuzzleShapes 함수 구현

---
+++ jest.setup.js
@@ -0,0 +1,1 @@
+// react-testing-library나 msw를 사용할거라면 여기에 테스트 셋업.
---
+++ src/socket-utils/parsePuzzleShapes.test.js
@@ -0,0 +1,6 @@
+import { parsePuzzleShapes, SAMPLE } from "./parsePuzzleShapes";
+
+test("parsePuzzleShapes function Test", () => {
+  const result = parsePuzzleShapes(SAMPLE);
+  console.log(result);
+});
