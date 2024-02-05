6f638c2aaa3078612684a910b0afa33ac7b7e00f
dlwndus0728 <dlwndus0728@ajou.ac.kr>
2024-02-05 14:45:35 +0900

    fix: localhost 수정

--- src/apis/requestBuilder.js
+++ src/apis/requestBuilder.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const request = axios.create({
-  // baseURL: "http://localhost:8080",
-  baseURL: "http://localhost:8082",
+  baseURL: "http://localhost:8080",
+  // baseURL: "http://localhost:8082",
   timeout: 3000,
   headers: { "Content-Type": "application/json" },
 });
