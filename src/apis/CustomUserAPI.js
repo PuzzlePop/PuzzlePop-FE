9a102fffb81e640f9a6e576c70d240bb3723959a
조시훈 <kepler452k@naver.com>
2024-02-11 18:34:27 +0900

    feat: Add CustomAPI.js and Implement ProfilePage.jsx

---
+++ src/apis/CustomUserAPI.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+export const UserAPI = axios.create({
+    // baseURL: 'https://i10a304.p.ssafy.io/api',
+    baseURL: 'http://localhost:8080',
+    timeout: 5000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export const fetchUser = async (id) => {
+    const response = await UserAPI.get(`/user?id=${id}`);
+    return response.data;
+};
+
+export const fetchUsers = async () => {
+    const response = await UserAPI.get(`/user/list`);
+    return response.data;
+};
+
+export const searchUserByEmail = async (email) => {
+    const params = { email: email };
+    const response = await UserAPI.get(`/user/search/email`, { params: params });
+    return response.data;
+};
+
+export const searchUsersByNickName = async (nickname) => {
+    const params = { nickname: nickname };
+    const response = await UserAPI.get(`/user/search/nickname`, { params: params });
+    return response.data;
+};
+
+const ApiObject = {
+    UserAPI,
+    fetchUser,
+    fetchUsers,
+    searchUserByEmail,
+    searchUsersByNickName,
+};
+
+export default ApiObject;
