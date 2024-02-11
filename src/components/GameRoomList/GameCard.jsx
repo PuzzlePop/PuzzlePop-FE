cb6eb3573aea6a546d39423ce69c6a4310c14a09
나해란 <nh2545@naver.com>
2024-02-12 00:57:12 +0900

    fix: 게임 시작 된 방이면 게임 시작 된 방 알림 먼저

--- src/components/GameRoomList/GameCard.jsx
+++ src/components/GameRoomList/GameCard.jsx
@@ -60,15 +60,18 @@ export default function GameCard({ room, category }) {
         setSnackOpen(true);
       }
       if (isAxiosError(e) && e.response.status === 403) {
-        setSnackMessage("정원이 가득찬 방입니다! ㅠㅠ");
+        setSnackMessage("정원이 가득찬 방입니다!");
         setSnackOpen(true);
       }
     }
   };
 
   const handleClick = (event, started) => {
-    if (redTeam.players.length + blueTeam.players.length === roomSize) {
-      setSnackMessage("정원이 가득찬 방입니다! ㅠㅠ");
+    if (started) {
+      setSnackMessage("이미 게임이 시작된 방입니다!");
+      setSnackOpen(true);
+    } else if (redTeam.players.length + blueTeam.players.length === roomSize) {
+      setSnackMessage("정원이 가득찬 방입니다!");
       setSnackOpen(true);
       // alert("정원이 가득찬 방입니다! ㅠㅠ");
     } else if (!started) {
