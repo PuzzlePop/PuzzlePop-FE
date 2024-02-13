b187924c9ab4ec2017e1ec9c0ddc50b55a025b6e
Juyeori <dlwndus0728@ajou.ac.kr>
2024-02-13 21:18:02 +0900

    hotfix: add login user

--- src/pages/CooperationGame/CooperationGameWaitingPage.jsx
+++ src/pages/CooperationGame/CooperationGameWaitingPage.jsx
@@ -57,10 +57,20 @@ export default function CooperationGameWaitingPage() {
           type: "ENTER",
           roomId: getRoomId(),
           sender: getSender(),
+          isMember: getCookie("userId") ? true : false
         }),
       );
     });
   };
+
+  //쿠키 확인
+  function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) {
+      return parts.pop().split(';').shift();
+    }
+  }
 
   const initialize = async () => {
     try {
