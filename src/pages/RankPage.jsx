ddfcc30256ed298b356263c91fc5a6a8402850c0
김다인 <frambo1se@naver.com>
2024-02-16 01:12:41 +0900

    feat: 승률 소수점 반올림, 비로그인 시 메인으로 보내는 로직 제거

--- src/pages/RankPage.jsx
+++ src/pages/RankPage.jsx
@@ -148,7 +148,11 @@ export default function RankPage() {
       case "winningRate":
         return (
           <>
-            <h1>{(ranking.win_count / ranking.played_game_count) * 100}%</h1>
+            <h1>
+              {Number.isInteger((ranking.win_count / ranking.played_game_count) * 100) ?
+              (ranking.win_count / ranking.played_game_count) * 100 : ((ranking.win_count /
+              ranking.played_game_count) * 100).toFixed(2)} %
+            </h1>
             WIN <b>{ranking.win_count}</b> | PLAY <b>{ranking.played_game_count}</b>
           </>
         );
